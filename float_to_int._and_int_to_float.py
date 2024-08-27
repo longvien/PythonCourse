a4473210ba44a1e8ea2873cdfb1a7be953804bd2
Long Vien <vienngoclongvn@gmail.com>
2024-08-27 15:29:13 +0200

    completed exercise

---
+++ float_to_int._and_int_to_float.py
@@ -0,0 +1,7 @@
+#examples
+#float to int
+amount = int(10.6)
+print(amount)
+#int to float
+amount = float(10)
+print(amount)
---
+++ tax_calculator.py
@@ -0,0 +1,9 @@
+#amount = 200
+#tax = 0.07
+#total = amount + amount*tax
+#print(total)
+
+amount = input[int("amount number?")]
+tax = input[int("tax number?")]
+total = amount + amount*tax
+print(total)
