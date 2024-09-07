2223ca1a28cd7dc968afa2a06f9ffc033e414617
Long Vien <vienngoclongvn@gmail.com>
2024-09-07 22:42:31 +0200

    new exercise

---
+++ check_if_i_have_shorter_lessons_or_not.py
@@ -0,0 +1,5 @@
+temperature = int(input("Wie heißt ist es heute?\n"))
+if temperature <= 30:
+    print("Wir haben heute keine verkürztem Unterricht :(")
+else:
+    print("Wir haben heute verkürztem Unterricht :)))")
