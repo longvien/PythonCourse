3d33d1c98f95544b47957302b7f3f830b03f6eca
Long Vien <vienngoclongvn@gmail.com>
2025-01-11 20:21:29 +0100

    new project

---
+++ Loops_with_range().py
@@ -0,0 +1,8 @@
+expenses = []
+total = 0
+num_expenses = int(input('Please enter an number of expenses\n'))
+for i in range(num_expenses):
+    expenses.append(float(input('Enter an expense\n')))
+total = sum(expenses)
+print('You spent $', total, sep = "")
+   
