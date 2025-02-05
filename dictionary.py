38c9d3bc45cbeacc56a4903f234c229c8405ed64
Long Vien <vienngoclongvn@gmail.com>
2025-02-05 19:03:44 +0100

    new code

--- dictionary.py
+++ dictionary.py
@@ -17,15 +17,3 @@
 #    print("Right! It means: ", definition)
 #else:
 #    print("The key you choose doesn't exist in this code:))))))))")
-
-movies = {
-    'Harry Potter and the Chamber of Secret': '11:30',
-    'Rush Hour 3': '12:10',
-    'Home Alone 1': '12:50',
-    'Harry Potter and the Half Blood Prince': '13:20'
-}
-movies_b = input("These are the movie that we will play today. Please choose one.")
-if movies_b in movies:
-    print(movies_b + ' plays at ' + movies.get(movies_b))
-else:
-    print(("The movie you entered doesn't exist. Please watch another movie"))
