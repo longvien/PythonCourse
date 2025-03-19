f6cf45e5471756c961f991c0d71ec5e9b264cdcd
Long Vien <vienngoclongvn@gmail.com>
2025-03-19 19:59:33 +0100

    fast commit

--- Loops_with_range().py
+++ Loops_with_range().py
@@ -1,5 +1,5 @@
 expenses = []
-total = 0
+total = 10
 num_expenses = int(input('Please enter an number of expenses\n'))
 for i in range(num_expenses):
     expenses.append(float(input('Enter an expense\n')))
