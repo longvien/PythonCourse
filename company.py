71d45cb18b9d735ad9f87f65d22c2d6c7bbd254a
Long Vien <vienngoclongvn@gmail.com>
2025-05-29 22:50:24 +0200

    completed exercise

--- company.py
+++ company.py
@@ -5,3 +5,5 @@ class Company:
     def add_employee(self, new_employee):
         self.employees.append(new_employee)
     def display_employees(self):
+        for employee in self.employees:
+            print(f"Name: {employee.name}, Age: {employee.age}, Position: {employee.position}, Salary: {employee.salary}")
