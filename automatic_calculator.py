a2b325055971cec392fc29c94626e5fe5ee654a3
Long Vien <vienngoclongvn@gmail.com>
2025-06-05 22:31:44 +0200

    bug problem unfixed, DNF

--- automatic_calculator.py
+++ automatic_calculator.py
@@ -1,11 +1,13 @@
 calculation = input('Please choose a calculation symbol - + : * ex: choose - if u need minus, + when u need plus!\n')
 if calculation == '+':
-    def a(a, b):
-        return a + b
+    def a(a, c):
+        return a + c
     first_num = float(input('Please enter the first number:\n'))
+    if not first_num == int or float:
+        print('Invalid input! Please enter numbers only.')
     second_num = float(input('Please enter the second number:\n'))
     result = a(first_num, second_num)
-    print('The result of', first_num, calculation, second_num, 'is', result)
+    print('The result of', first_num, calculation, second_num, 'is', result)      
 elif calculation == '-':
     def a(a, b):
         return a - b
