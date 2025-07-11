e9f19da43f7f70de849ffc4c241a4b8ddb355b3e
Long Vien <vienngoclongvn@gmail.com>
2025-07-11 21:16:39 +0200

    project shorter lesson checker upgrade

--- check_if_i_have_shorter_lessons_or_not.py
+++ check_if_i_have_shorter_lessons_or_not.py
@@ -1,5 +1,9 @@
-temperature = int(input("Wie heißt ist es heute?\n"))
-if temperature <= 40:
-    print("Wir haben heute keine verkürztem Unterricht :(")
+temperature = input("Is the temperature higher or reach 25 Celcius today?\n")
+if temperature == 'yes' or temperature == 'YES' or temperature == 'Yes' or temperature == 'Y' or temperature == 'y':
+    time = int(input("When does it reach 25 Celcius? ex: 12 o'clock = 12 \n"))
+    if time <= 12:
+        print('You have shorter lessons today!')
+    else:
+        print("You don't have shorter lessons today!")
 else:
-    print("Wir haben heute verkürztem Unterricht :)))")
+    print("You don't have shorter lessons today!")
