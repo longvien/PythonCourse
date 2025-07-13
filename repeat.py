a6afd59760511c817a79a27af3efd6c60ca46237
Long Vien <vienngoclongvn@gmail.com>
2025-07-13 22:06:48 +0200

    completed exercise

--- repeat.py
+++ repeat.py
@@ -1,6 +1,6 @@
 import random
 computer_choice = random.choice(['rock', 'paper', 'scissors'])
-player_choice  = input('Enter your choice: rock, papaer or scissors?\n')
+player_choice  = input('Enter your choice: rock, paper or scissors?\n')
 if player_choice == computer_choice:
     print('Draw!')
 elif computer_choice == 'rock' and  player_choice == 'paper':
