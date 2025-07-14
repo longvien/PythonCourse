8126813e302f7d597e15188e70b8ef1ad4887a2a
Long Vien <88818308+longvien@users.noreply.github.com>
2025-07-14 10:47:52 +0200

    fixed own basic error repeat.py

--- repeat.py
+++ repeat.py
@@ -2,7 +2,7 @@ import random
 computer_choice = random.choice(['rock', 'paper', 'scissors'])
 player_choice  = input('Enter your choice: rock, paper or scissors?\n')
 if player_choice == computer_choice:
-    print('Draw!, computer choice is", computer_choice)
+    print('Draw!, computer choice is', computer_choice)
 elif computer_choice == 'rock' and  player_choice == 'paper':
     print('You win! Computer choice is', computer_choice)
 
