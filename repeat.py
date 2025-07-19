dab424154cddaee1dfbd49522776d3e0f2780a5a
Long Vien <vienngoclongvn@gmail.com>
2025-07-19 21:53:58 +0200

    append repeat

--- repeat.py
+++ repeat.py
@@ -1,8 +1,3 @@
-import random
-computer_choice = random.choice(['rock', 'paper', 'scissors'])
-player_choice  = input('Enter your choice: rock, paper or scissors?\n')
-if player_choice == computer_choice:
-    print('Draw!')
-elif computer_choice == 'rock' and  player_choice == 'paper':
-    print('You win!')
-
+a = ['a', 'b', 'c', 'd', 'f']
+a.append('apple')
+print(a)
