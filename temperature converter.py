943396a1010452b31fe1b91098d0d63577b4524f
Long Vien <vienngoclongvn@gmail.com>
2025-07-21 22:36:43 +0200

    refactor temperatur converter

--- temperature converter.py
+++ temperature converter.py
@@ -14,15 +14,19 @@ def convert_F_C(number):
 def convert_C_F(number):  
     return (number * 9 / 5) + 32  # Correct Celsius to Fahrenheit formula
 
-def main():
-    choice = input('In which direction would u like to convert? °F to °C or conversely?\n')
 
-    if choice == '°F to °C' or choice == 'F to C':
-        number_1 = float(input('Please type in a number that you want to convert to celcius.\n'))
-        fahrenheit_to_celcius = convert_F_C(number_1)
-        print(number_1, 'is equal to', fahrenheit_to_celcius, '°C')
-    elif choice == '°C to °F' or choice == 'C to F':
-        number_2 = float(input('Please type in a number that you want to convert to fahrenheit.\n'))
-        celcius_to_fahrenheit = convert_C_F(number_2)
-        print(number_2, 'is equal to', celcius_to_fahrenheit, '°F')
-main()
+choice = input('In which direction would u like to convert? °F to °C or conversely?\n')
+if choice == '°F to °C' or choice == 'F to C':
+    measure = '°C'
+elif choice == '°C to °F' or choice == 'C to F':
+    measure = '°F'
+choices = {
+    '°F to °C' and 'F to C': convert_F_C,
+    '°C to °F' and 'C to F': convert_C_F
+}
+if choice in choices:
+    number = float(input('Please type in a number that you want to convert from ' + choice + '.\n'))
+    result = choices[choice](number)
+    print('The result after convert', number, 'from', choice, 'is: ' + str(result) + measure)
+else:
+    print('Invalid choice. Please select a valid conversion option.')
