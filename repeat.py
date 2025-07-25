48c32524e4009afc6b7b430b55df0b9ab409be70
Long Vien <vienngoclongvn@gmail.com>
2025-07-25 22:10:56 +0200

    re learn json, api

--- repeat.py
+++ repeat.py
@@ -1,20 +1,11 @@
-city = input('Please enter a city that u want to see the weather forecast!\n')
+
+
+
+
+city = input('Pls enter a city that u want to see the weather forecast\n')
 import requests
-url = 'http://api.weatherapi.com/v1/current.json?key=49e46da730864097922165451250402&q=' + city + '&aqi=no'
-response = requests.get(url)
-weather_json = response.json()
-
-
-temp = weather_json.get('current').get('temp_c')
-condition = weather_json.get('current').get('condition').get('text')
-print("It's now in", city, temp, "°C and", condition)
-
-
-
-city = input('Please enter a city that u want to see the weather forecast!\n')
-import requests
-url = 'http://api.weatherapi.com/v1/current.json?key=49e46da730864097922165451250402&q=' + city + '&aqi=no'
-response = requests.get(url)
-weather_json = response.json()
-temp = weather_json.get('current').get('temp_c')
+link = 'http://api.weatherapi.com/v1/current.json?key=49e46da730864097922165451250402&q=' + city + '&aqi=no'
+response = requests.get(link)
+data = response.json()
+temp = data.get('current').get('temp_c')
 print("It's now in", city, temp, "°C")
