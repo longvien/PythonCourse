64ae9f53aba918cd6878a6b4ed9491162e2da759
Long Vien <88818308+longvien@users.noreply.github.com>
2025-07-26 23:52:07 +0200

    fast commit

--- repeat.py
+++ repeat.py
@@ -1,10 +1,7 @@
 
-
-
-
 city = input('Pls enter a city that u want to see the weather forecast\n')
 import requests
-link = 'http://api.weatherapi.com/v1/current.json?key=49e46da730864097922165451250402&q=' + city + '&aqi=no'
+link = "http://api.weatherapi.com/v1/current.json?key=49e46da730864097922165451250402&q=" + city + "&aqi=no"
 response = requests.get(link)
 data = response.json()
 temp = data.get('current').get('temp_c')
