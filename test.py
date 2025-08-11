2c9c5a6ec29923687467cd6d2567f9b66268af6d
Long Vien <vienngoclongvn@gmail.com>
2025-08-11 19:01:05 +0200

    fast commit

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-class Gretting:
-    def __init__(self, f_name, l_name):
-        self.f_name = f_name
-        self.l_name = l_name
-    def gretting(self):
-        print('Hi', self.f_name, self.l_name)
-my_name = Gretting('Long', 'Vien')
-my_name.gretting()
+def greeting():
+    print('Hi', name + '!')
+name = input("What's your name?\n")
+greeting()
