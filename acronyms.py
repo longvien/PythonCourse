24bb6a137d87efd41c00d5fa1e0d857724db163b
Long Vien <vienngoclongvn@gmail.com>
2025-08-19 19:25:20 +0700

    test

--- acronyms.py
+++ acronyms.py
@@ -19,7 +19,7 @@ def add_acronym():
     acronym = input('Which acronym would u like to input?\n')
     definition = input("What's the definition of the acronym?\n")
     with open('input.txt', 'w') as file:
-        file.write(acronym + ' -- ' + definition + '\n')
+        file.write(acronym + ' - ' + definition + '\n')
 
 
 def main():
