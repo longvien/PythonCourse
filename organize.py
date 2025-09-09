074c61e23e4d3d16827f136e630b3e9a432d087f
Long Vien <vienngoclongvn@gmail.com>
2025-09-09 21:24:53 +0200

    completed course

---
+++ organize.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+desktop_path = os.path.expanduser("~/Desktop")
+
+# Define folders and extensions
+folders = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"],
+    "Documents": [".pdf", ".doc", ".docx", ".txt", ".ppt", ".pptx", ".xls", ".xlsx"],
+    "Archives": [".zip", ".rar", ".tar", ".gz", ".7z"],
+}
+
+# Create folders if they don't exist
+for folder_name in folders:
+    folder_path = os.path.join(desktop_path, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+# Move files into the right folder
+for file_name in os.listdir(desktop_path):
+    original_file_path = os.path.join(desktop_path, file_name)
+    if os.path.isfile(original_file_path):
+        for folder_name, extensions in folders.items():
+            for extension in extensions:
+                if file_name.endswith(extension):
+                    destination_folder = os.path.join(desktop_path, folder_name)
+                    print(original_file_path, destination_folder)
+                    #shutil.move(original_file_path, destination_folder)
