f38cda85a98f2673e463c0d6a17a82218fd45f07
Long Vien <vienngoclongvn@gmail.com>
2025-09-11 22:01:45 +0200

    recheck

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ menu = {'Breakfast: ': ['a', 'b', 'c'],
         'Lunch: ': ['d', 'e', 'f'],
         'Dinner: ': ['g', 'h', 'i']}
 for name, food in menu.items():
-    print(name, food)
+    print(food)
