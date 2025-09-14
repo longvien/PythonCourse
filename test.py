b11a75213435dab0bb42b39781241dac7fed05aa
Long Vien <vienngoclongvn@gmail.com>
2025-09-14 23:01:47 +0200

    completed exercise

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-menu = ['Breakfast: ' == ['a', 'b', 'c'],
-        'Lunch: ' == ['d', 'e', 'f'],
-        'Dinner: ' == ['g', 'h', 'i']]
-print(menu)
+menu = {
+    'Breakfast': ['a', 'b', 'c'],
+    'Lunch': ['d', 'e', 'f'],
+    'Dinner': ['g', 'h', 'i']
+}
+
+for a, i in menu.items():
+    print(a, ':', i)
+
+menu = [
+    ('Breakfast', ['a', 'b', 'c']),
+    ('Lunch', ['d', 'e', 'f']),
+    ('Dinner', ['g', 'h', 'i'])
+]
+print(menu[0][1])
