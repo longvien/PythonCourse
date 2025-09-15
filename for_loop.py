d296b0aeb72aee674eab322baf9e78a3b8f56d2a
Long Vien <vienngoclongvn@gmail.com>
2025-09-15 22:18:39 +0200

    fast modified

--- for_loop.py
+++ for_loop.py
@@ -3,6 +3,6 @@
 # Output: ['banana', 'mango', 'cherry']
 # What if we want to print each fruit on a separate line? We need a loop! 
 fruits = ['watermelon', 'cherry']
+
 for i in range(5):#added by myself
-    for fruit in fruits:
-        print(fruit)
+    print(fruits[0])
