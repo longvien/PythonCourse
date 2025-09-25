f8d9dd475d5b39caea1ba44052f210bf1dd1517d
Long Vien <vienngoclongvn@gmail.com>
2025-09-25 22:35:33 +0200

    completed exericse

---
+++ automatic_^_calculator.py
@@ -0,0 +1,10 @@
+class Calculation:
+    def __init__(self, first_num, second_num):
+        self.first = first_num
+        self.second = second_num
+    def calculate(self):
+        self.nums = []
+        for i in range(self.second):
+            self.nums.append(self.first)
+        for i in self.nums():
+                
