8ec221e58fc8ebc309a88658044ee11b9e280a7f
Long Vien <vienngoclongvn@gmail.com>
2025-10-07 23:06:34 +0200

    code adjustment

--- automatic_^_calculator.py
+++ automatic_^_calculator.py
@@ -2,7 +2,7 @@ class Calculation:
     def __init__(self, first_num, second_num):
         self.first = first_num
         self.second = second_num
-    def calculate(self):
+    def calculate(self, first_num, second_num):
         return self.first + self.second
     def display(self):
         print(calculate)
@@ -10,6 +10,7 @@ class Calculation:
 first_num = int(float(input('Pls enter the first number:\n')))
 second_num = int(float(input('Pls enter the second number:\n')))
 calculate = Calculation(first_num, second_num)
+calculate.calculate(first_num, second_num) # type: ignore
 
 
                 
