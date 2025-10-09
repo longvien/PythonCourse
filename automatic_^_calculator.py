67e73801e88bd5ddd38697433f10088518be3a49
Long Vien <vienngoclongvn@gmail.com>
2025-10-09 22:21:44 +0200

    bugs

--- automatic_^_calculator.py
+++ automatic_^_calculator.py
@@ -4,9 +4,6 @@ class Calculation:
         self.second = second_num
     def calculate(self, first_num, second_num):
         return self.first + self.second
-    def display(self):
-        print(calculate)
-
 first_num = int(float(input('Pls enter the first number:\n')))
 second_num = int(float(input('Pls enter the second number:\n')))
 calculate = Calculation(first_num, second_num)
