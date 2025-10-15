cccd9221403a6cf15ba9215a8d5691e506fbee6f
Long Vien <vienngoclongvn@gmail.com>
2025-10-15 21:32:17 +0200

    demo automatic calculator using class

---
+++ automatic_calculator_class.py
@@ -0,0 +1,53 @@
+class Calculation:
+    def __init__(self, first_num, second_num):
+        self.first = first_num
+        self.second = second_num
+    
+    def calculate(self):
+        return self.first + self.second
+
+first_num = int(float(input('Pls enter the first number:\n')))
+second_num = int(float(input('Pls enter the second number:\n')))
+calculate = Calculation(first_num, second_num)
+print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
+
+
+# class Calculation:
+#     def __init__(self, first_num, second_num):
+#         self.first = first_num
+#         self.second = second_num
+#     def calculate(self):
+#         return self.first + self.second
+
+# first_num = int(float(input('Pls enter the first number:\n')))
+# second_num = int(float(input('Pls enter the second number:\n')))
+# calculate = Calculation(first_num, second_num)
+# print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
+
+
+# class Calculation:
+#     def __init__(self, first_num, second_num):
+#         self.first = first_num
+#         self.second = second_num
+#     def calculate(self):
+#         return self.first + self.second
+
+# first_num = int(float(input('Pls enter the first number:\n')))
+# second_num = int(float(input('Pls enter the second number:\n')))
+# calculate = Calculation(first_num, second_num)
+# print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
+
+
+# class Calculation:
+#     def __init__(self, first_num, second_num):
+#         self.first = first_num
+#         self.second = second_num
+#     def calculate(self):
+#         return self.first + self.second
+
+# first_num = int(float(input('Pls enter the first number:\n')))
+# second_num = int(float(input('Pls enter the second number:\n')))
+# calculate = Calculation(first_num, second_num)
+# print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
+
+
