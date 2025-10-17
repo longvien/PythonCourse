2ef2482c134e39524a3cfd90a71b259aef07be51
Long Vien <vienngoclongvn@gmail.com>
2025-10-17 22:31:22 +0200

    update automatic calculator using class

--- automatic_calculator_class.py
+++ automatic_calculator_class.py
@@ -3,12 +3,15 @@ class Calculation:
         self.first = first_num
         self.second = second_num
     
+class Addition(Calculation):
+    def __init__(self, first_num, second_num):
+        super().__init__(first_num, second_num)
     def calculate(self):
         return self.first + self.second
 
 first_num = int(float(input('Pls enter the first number:\n')))
 second_num = int(float(input('Pls enter the second number:\n')))
-calculate = Calculation(first_num, second_num)
+calculate = Addition(first_num, second_num)
 print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
 
 
