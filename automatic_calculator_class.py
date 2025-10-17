ea93867eee2f8260e3996b3262c8ff095b7560d4
Long Vien <vienngoclongvn@gmail.com>
2025-10-17 22:51:28 +0200

    completed demo automatic calculator using class inheritance

--- automatic_calculator_class.py
+++ automatic_calculator_class.py
@@ -8,49 +8,39 @@ class Addition(Calculation):
         super().__init__(first_num, second_num)
     def calculate(self):
         return self.first + self.second
+class Subtraction(Calculation):
+    def __init__(self, first_num, second_num):
+        super().__init__(first_num, second_num)
+    def calculate(self):
+        return self.first - self.second
+class Multiplication(Calculation):
+    def __init__(self, first_num, second_num):
+        super().__init__(first_num, second_num)
+        if second_num == 0:
+            raise ZeroDivisionError("Can't be divided by zero")
+    def calculate(self):
+        return self.first * self.second
+class Division(Calculation):
+    def __init__(self, first_num, second_num):
+        super().__init__(first_num, second_num)
+    def calculate(self):
+        return self.first / self.second
 
-first_num = int(float(input('Pls enter the first number:\n')))
-second_num = int(float(input('Pls enter the second number:\n')))
-calculate = Addition(first_num, second_num)
-print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
+cal_symbols = {
+    '+': Addition,
+    '-': Subtraction,
+    '*': Multiplication,
+    '/': Division
+}
 
-
-# class Calculation:
-#     def __init__(self, first_num, second_num):
-#         self.first = first_num
-#         self.second = second_num
-#     def calculate(self):
-#         return self.first + self.second
-
-# first_num = int(float(input('Pls enter the first number:\n')))
-# second_num = int(float(input('Pls enter the second number:\n')))
-# calculate = Calculation(first_num, second_num)
-# print('The result of', first_num, '+', second_num, 'is', calculate.calculate()
-
-
-# class Calculation:
-#     def __init__(self, first_num, second_num):
-#         self.first = first_num
-#         self.second = second_num
-#     def calculate(self):
-#         return self.first + self.second
-
-# first_num = int(float(input('Pls enter the first number:\n')))
-# second_num = int(float(input('Pls enter the second number:\n')))
-# calculate = Calculation(first_num, second_num)
-# print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
-
-
-# class Calculation:
-#     def __init__(self, first_num, second_num):
-#         self.first = first_num
-#         self.second = second_num
-#     def calculate(self):
-#         return self.first + self.second
-
-# first_num = int(float(input('Pls enter the first number:\n')))
-# second_num = int(float(input('Pls enter the second number:\n')))
-# calculate = Calculation(first_num, second_num)
-# print('The result of', first_num, '+', second_num, 'is', calculate.calculate())
-
-
+symbol = input('Pls choose a calculate symbol: + - * / \n')
+if symbol in cal_symbols:
+    first_num = int(float(input('Pls enter the first number:\n')))
+    second_num = int(float(input('Pls enter the second number:\n')))
+    calculate = cal_symbols[symbol](first_num, second_num)
+    # if calculate.calculate()%2:
+    #     print('The result of', first_num, symbol , second_num, 'is', int(calculate.calculate()))
+    # else:
+    print('The result of', first_num, symbol , second_num, 'is', float(calculate.calculate()))
+else:
+    print("The option you chose doesn't exist. Pls try again!")
